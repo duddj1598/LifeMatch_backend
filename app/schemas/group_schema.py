ab97e2ddfd9a636e390d67fc7963e8505a5f09cd
seongbin55228295-netizen <seongbin55228295@gmail.com>
2025-11-12 09:41:19 +0900

    Add GroupRead

--- app/schemas/group_schema.py
+++ app/schemas/group_schema.py
@@ -6,3 +6,13 @@ class GroupCreate(BaseModel):
     description: Optional[str] = None
     category: Optional[str] = None
     max_member: Optional[int] = 10
+
+class GroupRead(BaseModel):
+    id: str
+    group_name: str
+    description: Optional[str] = None
+    category: Optional[str] = None
+    max_member: int
+
+    class Config:
+        orm_mode = True
